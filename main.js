a4a647bd4e1f5c45ea701e1221433584c5d89812
Alexandre Koch <xandekoch.work@gmail.com>
2022-05-28 16:40:57 -0300

    scroll reveal added, bugfix, code restructuring

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 // Scroll
 function onScroll() {
   if (scrollY == 0) {
-    navigation.classList.remove("scroll");
+    navigation.classList.remove('scroll');
   } else {
-    navigation.classList.add("scroll");
+    navigation.classList.add('scroll');
   }
 }
 
 if (scrollY == 0) {
-  navigation.classList.remove("scroll");
+  navigation.classList.remove('scroll');
 } else {
-  navigation.classList.add("scroll");
+  navigation.classList.add('scroll');
 }
 
 // Menu
 function openMenu() {
-  document.body.classList.add("menu-expanded");
+  document.body.classList.add('menu-expanded');
 }
 
 function closeMenu() {
-  document.body.classList.remove("menu-expanded");
+  document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 700
+}).reveal(`
+#home .head,
+#home .content,
+#home .content img,
+#home.data,
+#services .head,
+#services .card,
+#about .head,
+#about .content,
+#about .content img,
+#contact .head,
+#contact .content,
+#contact .content img,
+footer`);
